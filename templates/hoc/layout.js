b4d398623e85a59597986364476104afe13cd268
Alessandro Bellini <alessandro@ilmente.it>
2019-03-26 17:01:24 +0100

    update favicons hrefs

--- templates/hoc/layout.js
+++ templates/hoc/layout.js
@@ -25,10 +25,10 @@ export default (Template) => withRouter((props) => {
                 <meta name="title" content={pageTitle} />
                 <meta name="description" content={process.env.site.description} />
                 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inconsolata|Pacifico" />
-                <link rel="apple-touch-icon" sizes="180x180" href="static/favicons/apple-touch-icon.png" />
-                <link rel="icon" type="image/png" sizes="32x32" href="static/favicons/favicon-32x32.png" />
-                <link rel="icon" type="image/png" sizes="16x16" href="static/favicons/favicon-16x16.png" />
-                <link rel="manifest" href="static/favicons/site.webmanifest" />
+                <link rel="apple-touch-icon" sizes="180x180" href="/static/favicons/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/static/favicons/favicon-16x16.png" />
+                <link rel="manifest" href="/static/favicons/site.webmanifest" />
                 <title>{pageTitle}</title>
             </Head>
             <AccentContext.Provider value={{ color, prevColor }}>
