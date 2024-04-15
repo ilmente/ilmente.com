d01fe8514f6b8e83ac2d4ead1200bdad97f616b6
Alessandro Bellini <alessandro@ilmente.it>
2024-04-15 15:50:14 +0200

    fix config

--- next.config.js
+++ next.config.js
@@ -1,19 +1,19 @@
 module.exports = {
-    exportPathMap: async (defaultPathMap) => ({
-        ...defaultPathMap,
-        '/resume.html': { page: '/resume' }
-    }),
+  exportPathMap: async (defaultPathMap) => ({
+    ...defaultPathMap,
+    '/resume.html': { page: '/resume' },
+  }),
 
-    env: {
-        site: {
-            name: 'ilmente.com',
-            description: 'eclectic fool, since 1986'
-        },
-        menus: {
-            main: [
-                { label: 'Home', href: '/' },
-                { label: 'Resume', href: '/resume' }
-            ]
-        }
-    }
-}
+  env: {
+    site: {
+      name: 'ilmente.com',
+      description: 'eclectic fool, since 1986',
+    },
+    menus: {
+      main: [
+        { label: 'Home', href: '/' },
+        { label: 'Resume', href: '/resume' },
+      ],
+    },
+  },
+};
